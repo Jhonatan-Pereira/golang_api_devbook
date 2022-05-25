a91671e931272b70bf989b4e2dbe66bddb53dc98
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2022-05-24 23:58:26 -0300

    feat: atualizar senha (incomplete)

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -60,4 +60,10 @@ var rotarUsuarios = []Rota{
 		Funcao:             controllers.BuscarSeguindo,
 		RequerAutenticacao: true,
 	},
+	{
+		URI:                "/usuarios/{usuarioId}/atualizar-senha",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.AtualizarSenha,
+		RequerAutenticacao: true,
+	},
 }
