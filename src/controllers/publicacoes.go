e3b203ddd47a95bc539e65a8d5b619b1eafb3f67
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-05-26 00:52:23 -0300

    feat: created entity and publication routes

---
+++ src/controllers/publicacoes.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "net/http"
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+func BuscarPublicao(w http.ResponseWriter, r *http.Request) {
+
+}
+func AtualizarPublicao(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeletarPublicao(w http.ResponseWriter, r *http.Request) {
+
+}
